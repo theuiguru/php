80038d6cadea9600c1cfec5b9faa8b7f73379be8
Chris Thomas <webmaster102@gmail.com>
2018-02-15 22:04:09 -0500

    added php basics files so far

---
+++ basics/index.php
@@ -0,0 +1,36 @@
+<?php
+// Variables
+// $daysInWeek = 7;
+// echo $text = 'There are ' . $daysInWeek . ' days in a week.';
+
+// Strings
+// $message = 'It\'s a lovely day today';
+// echo $message;
+// $name = 'Chris';
+// $message = $name . ' said "learning PHP is fun".';
+// echo $message;
+// echo "<a href=\"#\">Link</a>";
+// echo '<a href="http://www.google.com">Link</a>';
+
+// Integers and Floats
+// $pi = 3.141;
+// $bankBalance = 1.2e6;
+// echo 'Your bank balance is $' . $bankBalance;
+
+// Booleans
+// $fileIsUploaded = false;
+// if($fileIsUploaded) {
+// 	echo 'Your file was uploaded.';
+// }
+// var_dump(is_int('chris')); //false
+// var_dump(is_int(1)); //true
+
+// Array Basics
+// $names = ['chris', 'luke', 'dan'];
+// var_dump($names);
+// echo $names[0];
+// $names[] = 'ash';
+// var_dump($names);
+// echo $names[3];
+
+// Diving Into Arrays
